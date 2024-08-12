78b89a90e8a33a0a5ea58d05c674a5c315333062
varunvjr <varun.vjr@gmail.com>
2024-08-11 18:39:02 -0700

    intial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World we are going to deploy this code in CI/CD !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
